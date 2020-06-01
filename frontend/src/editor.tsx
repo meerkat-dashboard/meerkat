37457789fb3934318665cacbe5910927ceaac30d
StreatCodes <matt.r.streatfield@gmail.com>
2020-06-02 09:44:37 +1000

    Add feather icon sprite sheet Initial editor work

--- frontend/src/editor.tsx
+++ frontend/src/editor.tsx
@@ -1,9 +1,27 @@
-import { h } from 'preact';
+import { h, Fragment } from 'preact';
 import { RoutableProps, route } from 'preact-router';
 import { useState, useEffect } from 'preact/hooks';
 
 export function Editor(props: RoutableProps) {
-	return <div class="editor">
-		editor
+	return <Fragment>
+	<div class="dashboard">
+		dashboard
 	</div>
+	<div class="editor">
+		<div class="icons">
+			<p>hi</p>
+			<svg class="feather">
+				<use xlinkHref="/res/svgs/feather-sprite.svg#home"/>
+			</svg>
+		</div>
+		<div class="options">
+			<h3>Edit Dashboard</h3>
+			<select>
+				<option value="item-type">Card</option>
+				<option value="item-type">SVG</option>
+				<option value="item-type">Image</option>
+			</select>
+		</div>
+	</div>
+	</Fragment>
 }
