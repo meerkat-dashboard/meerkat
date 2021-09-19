71a30a6adf30448831be158cafead7e191014e3e
Ken Nguyen <ken@sol1.com.au>
2021-09-19 12:48:09 +1000

    displays dashboards

---
+++ cypress/integration/dashboard/read.spec.js
@@ -0,0 +1,21 @@
+describe('Dashboard read', () => {
+	beforeEach(() => {
+		// create dashboards
+		cy.request({
+			method: 'POST',
+			url: '/dashboard',
+			body: '{"title":"ABC","tags":[],"background":null,"elements":[],"globalMute":true,"variables":{},"okSound":"/dashboards-data/ok.mp3","criticalSound":"/dashboards-data/critical.mp3","warningSound":"/dashboards-data/warning.mp3","unknownSound":"/dashboards-data/unknown.mp3","upSound":"/dashboards-data/up.mp3","downSound":"/dashboards-data/down.mp3"}'
+		}).its('body').should('include', '{"slug":"abc"}')
+		cy.request({
+			method: 'POST',
+			url: '/dashboard',
+			body: '{"title":"SBS","tags":[],"background":null,"elements":[],"globalMute":true,"variables":{},"okSound":"/dashboards-data/ok.mp3","criticalSound":"/dashboards-data/critical.mp3","warningSound":"/dashboards-data/warning.mp3","unknownSound":"/dashboards-data/unknown.mp3","upSound":"/dashboards-data/up.mp3","downSound":"/dashboards-data/down.mp3"}'
+		}).its('body').should('include', '{"slug":"sbs"}')
+	})
+
+	it('displays dashboards', () => {
+		cy.visit('/')
+		cy.contains("ABC").should('be.visible')
+		cy.contains("SBS").should('be.visible')
+	})
+})
