71b835f5bcd0ce3e24e3e5c30719ad75971741f6
Dave Kempe <dave@sol1.com.au>
2022-08-11 10:23:03 +1000

    Update install with LABEL feature

--- contrib/install-ubuntu.sh
+++ contrib/install-ubuntu.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+LABEL=$1
+INSTALLDIR=/usr/local/meerkat$LABEL
+
 echo "Installing on ubuntu/debian. If it doesn't work, fix it."
 
 echo "run this script from the git repo like ./contrib/install-ubuntu.sh, as it assumes you are doing that"
+echo "You can pass in a LABEL as an argument to this script, which will append it to the path and startup script."
+echo "This allows you to have different versions (LABELs) of meerkat installed on the same machine."
+echo "You will have to modify the port in the config file yourself however."
 
 GOVERSION=`go version |awk '{print $3}'|sed 's/go//'`
 
@@ -21,9 +28,9 @@ apt install nodejs
 echo "installing meerkat"
 
 echo "stopping meerkat first!"
-systemctl stop meerkat
+systemctl stop meerkat$LABEL
 
-mkdir /usr/local/meerkat -p
+mkdir $INSTALLDIR -p
 
 echo "Building meerkat"
 cd backend
@@ -35,16 +42,16 @@ if [ $? -ne 0 ]
 fi
 
 chmod +x meerkat
-cp -a meerkat /usr/local/meerkat/
+cp -a meerkat $INSTALLDIR/
 
 cd ..
-cp -av dashboards-data /usr/local/meerkat/
-cp -av dashboards /usr/local/meerkat/
+cp -av dashboards-data $INSTALLDIR/
+cp -av dashboards $INSTALLDIR/
 
-if [ ! -f /etc/meerkat.toml ]; then
-        cp config/meerkat.toml.example /etc/meerkat.toml
+if [ ! -f /etc/meerkat$LABEL.toml ]; then
+        cp config/meerkat.toml.example /etc/meerkat$LABEL.toml
 else
-    echo "config already in place at /etc/meerkat.toml"
+    echo "config already in place at /etc/meerkat$LABEL.toml"
 fi
 
 cd frontend
@@ -53,20 +60,28 @@ npm run build
 
 cd ..
 
-rm -rf /usr/local/meerkat/frontend
-cp frontend /usr/local/meerkat/frontend -av
+rm -rf $INSTALLDIR/frontend
+cp frontend $INSTALLDIR/frontend -av
 
-chown nagios:nagios /usr/local/meerkat/ -R
+chown nagios:nagios $INSTALLDIR/ -R
 
-echo "installing meerkat service"
-cp contrib/meerkat.service /etc/systemd/system/meerkat.service
+echo "installing meerkat service call meerkat$LABEL.service"
+echo "
+[Unit]
+Description=meerkat$LABEL
+After=network.target
+
+[Service]
+WorkingDirectory=$INSTALLDIR/
+ExecStart=$INSTALLDIR/meerkat -config /etc/meerkat$LABEL.toml
+User=nagios
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/meerkat$LABEL.service
+
 systemctl daemon-reload
-systemctl restart meerkat
+systemctl restart meerkat$LABEL
 
-echo "you need to fix the meerkat config file before it will work in /etc/meerkat.toml"
+echo "you need to fix the meerkat config file before it will work in /etc/meerkat$LABEL.toml"
 echo "its currently like this"
-cat /etc/meerkat.toml
-
-
-
-
+cat /etc/meerkat$LABEL.toml
