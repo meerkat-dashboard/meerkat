fcdf576ad5b43809aeca2725f7f0249e04c1c934
Oliver Lowe <o@olowe.co>
2022-09-30 15:49:17 +1000

    Create a distribution tarball using github workflows
    We may as well have a system run this for us so that others can
    download it.

--- mkdist.sh
+++ mkdist.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
 
-# Creates a distribution tarball in a temporary directory from source.
-# The path to the created tarball is the last line of the script's
-# output.
+# Creates a distribution tarball in the named directory (or a
+# temporary directory if none specified) from source. The path to the
+# created tarball is the last line of the script's output.
+
+usage="usage: mkdist.sh [directory]"
+
+if test $# -gt 1
+then
+	echo $usage
+	exit 2
+fi
+
+tmp=`mktemp -d`
+outdir="$tmp"
+if test -n "$1"
+then
+	outdir="$1"
+fi
+
+if ! test -d "$outdir"
+then
+	echo "$outdir not a directory"
+	echo $usage
+	exit 1
+fi
 
 ( cd backend && go build )
 
@@ -12,25 +34,23 @@
 	npm run build
 )
 
-tmp=`mktemp -d`
-dir="$tmp/meerkat"
-mkdir -p $dir
+workdir="$tmp/meerkat"
+mkdir -p $workdir
 
-cp README.md LICENSE $dir
-cp -R dashboards-data $dir
-cp Dockerfile $dir
-cp -R contrib $dir
-cp -R docs $dir
+cp README.md LICENSE $workdir
+cp -R dashboards-data $workdir
+cp Dockerfile $workdir
+cp -R contrib $workdir
+cp -R docs $workdir
 
-cp config/meerkat.toml.example $dir
-cp backend/meerkat $dir
+cp config/meerkat.toml.example $workdir
+cp backend/meerkat $workdir
 
-mkdir -p "$dir/frontend"
-cp -R frontend/index.html frontend/style.css frontend/res frontend/assets "$dir/frontend"
-cp -R frontend/react-widgets.css frontend/fonts "$dir/frontend"
-cp -R frontend/dist "$dir/frontend"
+mkdir -p "$workdir/frontend"
+cp -R frontend/index.html frontend/style.css frontend/res frontend/assets "$workdir/frontend"
+cp -R frontend/react-widgets.css frontend/fonts "$workdir/frontend"
+cp -R frontend/dist "$workdir/frontend"
 
-tarball="$tmp/meerkat.tar.gz"
-cd "$tmp"
-tar cv meerkat | gzip -c > "$tarball"
+tarball="$outdir/meerkat.tar.gz"
+( cd "$workdir" && tar cv meerkat | gzip -c )  > "$tarball"
 echo "$tarball"
