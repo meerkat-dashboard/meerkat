0cbf92ba51e3f66bfcd9bfc5ca0de3f54da2f837
Oliver Lowe <o@olowe.co>
2022-10-14 20:27:22 +1100

    backend: remove http request tracing
    In development settings, we can open the web dev tools of the web
    browser we are developing against to see requests. In normal
    installations, we don't want so much being logged by default.

--- backend/main.go
+++ backend/main.go
@@ -7,11 +7,9 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/BurntSushi/toml"
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 	"github.com/mailgun/groupcache/v2"
 )
 
@@ -86,12 +84,6 @@ func main() {
 	cachepool.Set("http://localhost:8585")
 
 	r := chi.NewRouter()
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.Timeout(30 * time.Second))
 
 	r.Handle("/_groupcache/", cachepool)
 
