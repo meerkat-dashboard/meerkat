acf38c6080af217467286f1bfe34d866950dbcb9
Oliver Lowe <o@olowe.co>
2022-10-19 21:53:53 +1100

    frontend: Extract text alignment element options
    These options are used by both static and dynamic text elements, but
    each of them specifies it twice! Not imported by anything yet, and not
    fully functional.

--- frontend/src/elements/options.jsx
+++ frontend/src/elements/options.jsx
@@ -20,6 +20,7 @@ export function FontSizeInput({ value, onInput }) {
 }
 
 export function ExternalURL({ value, onInput }) {
+	console.debug("external URL input value is", value);
 	return (
 		<fieldset>
 			<label class="form-label" for="card-linking-url">
@@ -31,10 +32,60 @@ export function ExternalURL({ value, onInput }) {
 				name="external-url"
 				type="url"
 				placeholder="http://www.example.com"
-				value={value}
+				value={value ? value : ""}
 				onInput={onInput}
 			></input>
 			<small class="form-text">Clicking the element will load this URL.</small>
 		</fieldset>
+	);
+}
+
+export function AlignmentInput() {
+	return (
+		<Fragment>
+			<fieldset>
+				<legend>Horizontal alignment</legend>
+				<div class="form-check">
+					<input class="form-check-input" type="radio" name="horizontal" id="startAlign" />
+					<label class="form-check-label" for="startAlign">
+						Start
+					</label>
+				</div>
+				<div class="form-check">
+					<input class="form-check-input" type="radio" name="horizontal" id="centerAlign" />
+					<label class="form-check-label" for="centerAlign">
+						Centre
+					</label>
+				</div>
+				<div class="form-check">
+					<input class="form-check-input" type="radio" name="horizontal" id="endAlign" />
+					<label class="form-check-label" for="endAlign">
+						End
+					</label>
+				</div>
+			</fieldset>
+
+			<fieldset>
+				<legend>Vertical alignment</legend>
+				<div class="form-check">
+					<input class="form-check-input" type="radio" name="vertical" id="topAlign" />
+					<label class="form-check-label" for="topAlign">
+						Top
+					</label>
+				</div>
+				<div class="form-check">
+					<input class="form-check-input" type="radio" name="vertical" id="vcenterAlign" />
+					<label class="form-check-label" for="vcenterAlign">
+						Centre
+					</label>
+				</div>
+				<div class="form-check">
+					<input class="form-check-input" type="radio" name="vertical" id="bottomAlign" />
+					<label class="form-check-label" for="bottomAlign">
+						Bottom
+					</label>
+				</div>
+			</fieldset>
+		</Fragment>
 	);
 }
