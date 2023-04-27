meerkat-dashboard/meerkat

commit a1bfa742bf2b20d088d7088ebeb2186f1bedc9b5

ui: Render more of the dashboard viewer on the server
We don't need javascript to render static content; it's clearer as plain HTML.

ui/src/viewer.jsx
@@ -54,23 +54,7 @@ function Viewer({ dashboard, events }) {
 			</div>
 		);
 	});
-
-	const backgroundImage = dashboard.background ? dashboard.background : null;
-
-	return (
-		<div class="dashboard view-only">
-			{backgroundImage ? (
-				<img
-					src={backgroundImage}
-					style="height: 100%; width: 100%;"
-					id="dashboard-dimensions"
-				/>
-			) : (
-				<div style="height: 100vh; width: 100vw"></div>
-			)}
-			{elements}
-		</div>
-	);
+	return elements;
 }
 
 function IcingaElement({ typ, options, events }) {
@@ -91,15 +75,18 @@ function IcingaElement({ typ, options, events }) {
 
 	let interests = options.objectName;
 	if (options.objectType.endsWith("group")) {
-		meerkat.getAllInGroup(options.objectName, options.objectType).then((results) => {
-			for (const v of results) {
-				interests.push(v.name);
-			}
-		}).catch((err) => {
-			console.error(
-				`fetch ${options.objectType} ${options.objectName}: ${err}`
-			);
-		});
+		meerkat
+			.getAllInGroup(options.objectName, options.objectType)
+			.then((results) => {
+				for (const v of results) {
+					interests.push(v.name);
+				}
+			})
+			.catch((err) => {
+				console.error(
+					`fetch ${options.objectType} ${options.objectName}: ${err}`
+				);
+			});
 	}
 
 	function refresh() {
@@ -175,5 +162,8 @@ const elems = window.location.pathname.split("/");
 const slug = elems[elems.length - 2];
 const events = new EventSource("/icinga/stream");
 meerkat.getDashboard(slug).then((d) => {
-	render(<Viewer dashboard={d} events={events} />, document.body);
+	render(
+		<Viewer dashboard={d} events={events} />,
+		document.getElementById("dashboard")
+	);
 });

ui/pages.go
@@ -14,12 +14,24 @@ import (
 )
 
 func (srv *Server) ViewHandler(w http.ResponseWriter, req *http.Request) {
+	slug := path.Dir(req.URL.Path)
+	fname := path.Join("dashboards", slug+".json")
+	dashboard, err := meerkat.ReadDashboard(fname)
+	if errors.Is(err, fs.ErrNotExist) {
+		http.NotFound(w, req)
+		return
+	} else if err != nil {
+		msg := fmt.Sprintf("read dashboard %s: %v", slug, err)
+		log.Print(msg)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
 	tmpl, err := template.ParseFS(srv.fsys, "template/layout.tmpl", "template/view.tmpl")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, dashboard)
 }
 
 func (srv *Server) EditHandler(w http.ResponseWriter, req *http.Request) {

ui/src/elements/line.jsx
@@ -68,12 +68,9 @@ export function CheckLineOptions({ options, updateOptions }) {
 }
 
 export function CheckLine({ state, options }) {
-	const stateText = icinga.StateText(state, options.objectType)
+	const stateText = icinga.StateText(state, options.objectType);
 	return (
-		<svg
-			class={`check-content svg ${stateText}`}
-			stroke-linecap="rounded"
-		>
+		<svg class={`check-content svg ${stateText}`} stroke-linecap="rounded">
 			<marker
 				class={stateText}
 				id="arrow"

ui/style.css
@@ -34,6 +34,8 @@
 }
 
 .dashboard.view-only {
+	width: 100vw;
+	height: 100vh;
 	user-select: none;
 	position: relative;
 	flex-grow: 1;
@@ -156,7 +158,7 @@
 }
 
 .card.ok, .card.up {
-	background-color: lime;
+	background-color: limegreen;
 }
 
 .card.warning {
@@ -172,11 +174,11 @@
 }
 
 svg.ok {
-	stroke: lime;
+	stroke: limegreen;
 }
 
 line.ok, marker.ok {
-	fill: lime;
+	fill: limegreen;
 }
 
 svg.warning {

ui/template/view.tmpl
@@ -5,6 +5,16 @@
 	<link rel="stylesheet" href="/dist/style.css">
 	<link rel="stylesheet" href="/dist/bootstrap.min.css">
 	<script src="/dist/viewer.js" defer></script>
-	<title>Meerkat</title>
+	<style>
+	.dashboard {
+		background-image: url("{{.Background}}");
+		background-repeat: no-repeat;
+	}
+	</style>
+	<title>{{.Title}}</title>
 {{ end }}
+
+{{define "body"}}
 <!-- preact ../src/viewer.jsx  -->
+<div id="dashboard" class="dashboard view-only"></div>
+{{end}}

commit e60ec025219cf7b042b9dc3459692ee47b7ab3a1

ui: restore previous element positioning on no background
This was broken by a1bfa742bf2b20. Only partially resolved by
ccc28b91b. We need to create a div that takes up the entire viewport,
then position elements relative to it. The way all this works is a
hack, but hey!

ui/src/viewer.jsx
@@ -66,7 +66,7 @@ function Viewer({ dashboard, events }) {
 			</div>
 		);
 	}
-	return { elements };
+	return <div style="width: 100vh; height: 100vh">{elements}</div>;
 }
 
 function IcingaElement({ typ, options, events }) {