10d3cc228b61f6486c9f966e2b983f756e199ffb
Matthew Smith <matthew.smith@sol1.com.au>
2023-05-31 14:00:18 +1000

    160 folders and other dashboard nav (#181)
    Adds in dashboard description, dashboard folder and nav changes to the index to make it easer to find, sort and know what a dashboard is for. #160
    
    Requires an update to the migrator for existing dashboards. #180

--- dashboard.go
+++ dashboard.go
@@ -14,12 +14,14 @@ import (
 
 // Dashboard contains all information to render a dashboard
 type Dashboard struct {
-	Title      string    `json:"title"`
-	Slug       string    `json:"slug"`
-	Background string    `json:"background"`
-	Width      string    `json:"width"`
-	Height     string    `json:"height"`
-	Elements   []Element `json:"elements"`
+	Title       string    `json:"title"`
+	Slug        string    `json:"slug"`
+	Background  string    `json:"background"`
+	Description string    `json:"description"`
+	Folder      string    `json:"folder"`
+	Width       string    `json:"width"`
+	Height      string    `json:"height"`
+	Elements    []Element `json:"elements"`
 }
 
 // Element contains any service/host information needed
@@ -124,6 +126,10 @@ func ParseDashboardForm(form url.Values) (Dashboard, error) {
 			}
 		case "background":
 			dashboard.Background = v
+		case "description":
+			dashboard.Description = v
+		case "folder":
+			dashboard.Folder = v
 		default:
 			return Dashboard{}, fmt.Errorf("unknown form parameter %s", k)
 		}
